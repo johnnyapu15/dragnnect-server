72af7e6494bb90ec46977be909d624f015d5e333
johnnyapu15 <johnnyapu15@gmail.com>
2019-04-27 22:46:57 +0900

    c

--- static/js/2d-demo-test.js
+++ static/js/2d-demo-test.js
@@ -35,8 +35,8 @@ socket.on('demo-receive', function(data) {
 socket.on('demo-2d-line', function(data) {
     ctx_2d_demo.width = canvas_2d_demo.width;
     ctx_2d_demo.height = canvas_2d_demo.height;
-    ctx_2d_demo.clearRect(0, 0, ctx_2d_demo.width, ctx_2d_demo.height);
     ctx_2d_demo.restore();
+    ctx_2d_demo.clearRect(0, 0, ctx_2d_demo.width, ctx_2d_demo.height);
     translateMap();
 
     data['lines'].forEach(l => {
